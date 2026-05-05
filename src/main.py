df6c13fa80d2f78d7785404df43cc24ede9c9e39
icondado <i.condado.alcantarilla@gmail.com>
2026-05-05 11:43:28 +0200

    feat: modificar main scriptadd main script

--- src/main.py
+++ src/main.py
@@ -31,25 +31,33 @@ def get_data_list_from_join():
     """Obtener datos DA"""
     connection = conection_bd()
     with connection:
-        join_query_sql = """ SELECT 
-                c.customer_id,
-                c.first_name,
-                c.last_name,
-                c.email,
-                a.address,
-                ci.city,
-                co.country,
-                r.rental_id,
-                r.rental_date,
-                r.return_date,
-                p.payment_id,
-                p.amount
-            FROM customer AS c
-            JOIN address AS a ON c.address_id = a.address_id
-            JOIN city AS ci ON a.city_id = ci.city_id
-            JOIN country AS co ON ci.country_id = co.country_id
-            JOIN rental AS r ON c.customer_id = r.customer_id
-            JOIN payment AS p ON r.rental_id = p.rental_id; """
+        join_query_sql = """
+        SELECT 
+            c.customer_id,
+            LOWER(c.first_name) AS first_name,
+            LOWER(c.last_name) AS last_name,
+            LOWER(c.email) AS email,
+            LOWER(ci.city) AS city,
+            LOWER(co.country) AS country,
+            r.rental_id,
+            r.rental_date,
+            r.return_date,
+            DATEDIFF(r.return_date, r.rental_date) AS rental_duration_days,
+            p.payment_id,
+            p.amount,
+            p.payment_date
+        FROM customer c
+        JOIN address a ON c.address_id = a.address_id
+        JOIN city ci ON a.city_id = ci.city_id
+        JOIN country co ON ci.country_id = co.country_id
+        JOIN rental r ON c.customer_id = r.customer_id
+        JOIN payment p ON r.rental_id = p.rental_id
+        WHERE 
+            r.rental_id IS NOT NULL 
+            AND p.payment_id IS NOT NULL
+            AND p.amount > 0              
+            AND r.return_date IS NOT NULL 
+            AND r.rental_date < r.return_date; """
 
         # Ejecutar la consulta y capturar datos
         result = connection.execute(text(join_query_sql))
