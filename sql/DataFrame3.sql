a1150b6bc5bddc6eb01ebd4379a898a074e91d6e
icondado <i.condado.alcantarilla@gmail.com>
2026-05-06 05:56:12 +0200

    feat: modify Dataframe3

--- sql/DataFrame3.sql
+++ sql/DataFrame3.sql
@@ -5,10 +5,6 @@
     -- Objetivo: analizar el elenco por película y frecuencia de aparición de actores.
 --------------------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS elenco_final;
-DROP TABLE IF EXISTS actores_unicos;
-DROP TABLE IF EXISTS actores_limpios;
-
 USE sakila;
 
 show tables;
@@ -20,34 +16,40 @@ JOIN actor a ON fa.actor_id = a.actor_id;
 
 -- Limpieza ----------------------------------
 -- Estandarizar nombres de actores (LOWER(first_name) y LOWER(last_name)).
-
-CREATE TEMPORARY TABLE tmp_actores_limpios AS
-SELECT 
-    actor_id,
-    LOWER(TRIM(first_name)) AS first_name,
-    LOWER(TRIM(last_name)) AS last_name
-FROM actor;
-
 -- Eliminar actores duplicados (verificando combinaciones de nombre + apellido).
-
-CREATE TEMPORARY TABLE tmp_actores_unicos AS
-SELECT 
-    MIN(actor_id) AS actor_id, -- Nos quedamos con un solo ID por nombre
-    first_name,
-    last_name,
-    first_name || ' ' || last_name AS actor_full_name -- Creamos el nombre completo
-FROM tmp_actores_limpios
-GROUP BY first_name, last_name;
-
 -- Asegurar que film.film_id y actor.actor_id existan (joins consistentes).
 -- Filtrar películas sin actores asociados.
-CREATE TEMPORARY TABLE tmp_elenco_final AS
+-- Sugerencias adicionales ----------------------------------
+-- Generar tabla agregada con:
+-- número de actores por película
+-- número de películas por actor
+-- Crear columna derivada: actor_full_name.
+
+DROP TEMPORARY TABLE IF EXISTS tmp_elenco_enriquecido;
+
+CREATE TEMPORARY TABLE tmp_elenco_enriquecido AS
 SELECT 
     f.film_id,
     f.title,
-    au.actor_id,
-    au.actor_full_name
-FROM film f
-INNER JOIN film_actor fa ON f.film_id = fa.film_id
-INNER JOIN tmp_actores_unicos au ON fa.actor_id = au.actor_id;
+    a.actor_id,
+    CONCAT(LOWER(TRIM(a.first_name)), ' ', LOWER(TRIM(a.last_name))) AS actor_full_name,
 
+    (
+        SELECT COUNT(*) 
+        FROM film_actor fa2
+        WHERE fa2.film_id = f.film_id
+    ) AS num_actors_per_film,
+
+    (
+        SELECT COUNT(*) 
+        FROM film_actor fa3
+        WHERE fa3.actor_id = a.actor_id
+    ) AS num_films_per_actor
+
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+JOIN actor a ON fa.actor_id = a.actor_id;
+
+select * from tmp_elenco_enriquecido;
+
+select count(actor_id) From tmp_elenco_enriquecido;
